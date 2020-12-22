e5fe16bfe0b0ec41614324d2066fefcf2a38d4f5
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2020-12-22 17:48:58 +0200

    documented helper functions

--- config/helpers.php
+++ config/helpers.php
@@ -1,19 +1,55 @@
 <?php
-function view($view_name)
+
+/**
+ * include view file from ./views/pages/
+ * 
+ * @param $view_name(str): the file name in ./views/pages/
+ * without .php file extension
+ * 
+ * if file not exists return 500 error page
+ * 
+ * @return void
+ */
+function view($view_name): void
 {
   $path = './views/pages/' . $view_name . '.php';
   if (!file_exists($path))
-    abort(404);
+    abort(500);
   else
     require_once($path);
 }
 
-function abort($err)
+/**
+ * include error page
+ * 
+ * @param $err(int): the error number
+ * 
+ * if the error page not exist, returns string "Error 500 ..."
+ * 
+ * @return void
+ */
+function abort($err): void
 {
-  include_once('./views/errors/' . $err . '.php');
+  $path = './views/errors/' . $err . '.php';
+  if (!file_exists($path)) {
+    if ($err == 500)
+      return "Error 500 - Server Error";
+    abort(500);
+  } else
+    require_once($path);
 }
 
-function controller($controller_name)
+/**
+ * include controller 
+ * 
+ * @param $controller_name(str): the name of the controller in ./controllers/
+ * without .controller.php file extension
+ * 
+ * if file not exists return error 500
+ * 
+ * @return void
+ */
+function controller($controller_name): void
 {
   $path = './controllers/' . $controller_name . '.controller.php';
   if (!file_exists($path))
